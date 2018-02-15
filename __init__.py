45407028bb76e9bf201be7b2d76e6451c2afa7b9
Jeremie Gobeil <jeremie.gobeil@gmail.com>
2018-02-15 10:51:46 +0100

    Correction of PyCharm "optimization"

--- __init__.py
+++ __init__.py
@@ -1,5 +1,14 @@
 # .sxm file
+from .sxmfile import SxmFile
+from .topodataset import SxmDataSet
 
 # .dat file
+from .datfile import DatDataSet
+from .datfile import open_datfile
+from .datfile import BiasSpec
 
 # Tranfer function class and function
+from .s2pfile import TFDataSet
+from .s2pfile import open_s2p
+
+from .opener import Opener
