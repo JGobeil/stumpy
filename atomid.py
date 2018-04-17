558270d66bfe2c0b96dffa2bab4c94f66f066f2a
Jeremie Gobeil <jeremie.gobeil@gmail.com>
2018-04-17 15:25:07 +0200

    Better results plotting

--- atomid.py
+++ atomid.py
@@ -9,10 +9,10 @@ class _PtIdConfig(dict):
     class _AtomConfig(list):
         def __init__(self):
             super().__init__()
-    
+
         def add_point(self, bias, value):
             """ Add a point to the config.
-    
+
             Parameters
             ----------
             bias: tuple
@@ -22,27 +22,27 @@ class _PtIdConfig(dict):
                 this is use depend on the identification method.
             """
             self.append((*bias, *value))
-    
+
         def __repr__(self):
             return "\n".join([
             "%3d: %s" % (i, s) for i, s in enumerate(self)])
-    
+
     def __init__(self):
         super().__init__()
-    
+
     def create_config(self, atom):
         """ Create a new atom id config. Return the newly created
         config. Erase any existing config with this name.
-    
+
         Parameters
         ----------
         atom: str
             The name of the config.
-        
+
         Returns
         -------
         cfg: The newly create config
-        
+
         """
         self[atom] = _PtIdConfig._AtomConfig()
         return self[atom]
@@ -51,7 +51,7 @@ class _PtIdConfig(dict):
 # The Average Id Config database
 average_id_config = _PtIdConfig()
 
-# create a new 'type' of atom    
+# create a new 'type' of atom
 _fe_config = average_id_config.create_config(atom="Fe")
 # we can get it (if exist)
 _fe_config = average_id_config["Fe"]
@@ -59,10 +59,10 @@ _fe_config = average_id_config["Fe"]
 _fe_config.add_point(bias=(-0.0180, -0.0120), value=(0.6, 1.00))
 _fe_config.add_point(bias=(-0.0025, -0.0010), value=(0.0, 0.40))
 _fe_config.add_point(bias=(-0.0005, +0.0005), value=(0.0, 0.05))
-_fe_config.add_point(bias=(+0.0001, +0.0025), value=(0.0, 0.40))
+_fe_config.add_point(bias=(+0.0010, +0.0025), value=(0.0, 0.40))
 _fe_config.add_point(bias=(+0.0120, +0.0180), value=(0.6, 1.00))
 
-    
+
 slope_id_config = _PtIdConfig()
 _fe_config = slope_id_config.create_config(atom="Fe")
 _fe_config.add_point(bias=(-0.0170, -0.0070), value=(0.85, 1.00))
@@ -72,6 +72,14 @@ _fe_config.add_point(bias=(+0.0070, +0.0170), value=(0.85, 1.00))
 
 
 class _AtomId:
+    def __init__(self, specs):
+        self.methods = None
+        self.specs = specs
+
+    @property
+    def is_multimethods(self):
+        return self.methods is not None
+
     def plot(self, channel, radius=0.8,
              colors=["Green", "Yellow", "Red"],
              with_number=False,
@@ -79,10 +87,10 @@ class _AtomId:
         ax = channel.plot(**kwargs)
         topo = channel.topo
 
-        if len(self.identified.shape) == 1:
-            identified = np.array([self.identified, self.identified]).T
-        else:
+        if self.is_multimethods:
             identified = self.identified
+        else:
+            identified = np.array([self.identified, self.identified]).T
 
         for spec, identified in zip(self.specs, identified):
             xy = topo.abs2topo(spec.xyz_nm[0:2])
@@ -103,36 +111,63 @@ class _AtomId:
                         fontdict={'color': c, 'weight': "bold", 'size': 15}
                         )
         return ax
-        
-    def plot_results(self):
-        conditions = self.conditions
-        points = [(c[0]+c[1])/2 for c in conditions]
-    
+
+    def plot_results(self, boxcolor='0.9'):
+        if self.is_multimethods:
+            methods = self.methods
+        else:
+            methods = [self, ]
+
+        ncols = len(methods) + 2
         figs = []
+
         for i, s in enumerate(self.specs):
-            fig, axis = plt.subplots(ncols=3, figsize=(15, 5))
+            fig, axis = plt.subplots(ncols=ncols, figsize=(15, 5))
+
+            if not self.is_multimethods:
+                for c in methods[0].conditions:
+                    axis[0].axvspan(c[0], c[1], color='0.8', alpha=0.5)
+
             s.data.plot(s.keys.V, s.keys.dIdV, ax=axis[0])
-    
-            colors = ['Green' if pt else 'Red' for pt in self.pts_ok[i]]
-    
-            axis[1].scatter([1,2,3,4,5], self.pts_value[i], color=colors)
-            for c in conditions:
-                axis[1].axhline(c[2], linestyle='dashed')
-                axis[1].axhline(c[3], linestyle='dashed')
-    
-            axis[1].set_ylim(-0.5, 1.5)
-    
-            axis[2].text(0.5, 0.5, self.identified[i])
-            no_ticks(axis[2])
-            no_axis(axis[2])
-            
+            axis[0].set_title(s.serie_number)
+            axis[0].legend().remove()
+
+            for j, mtd in enumerate(methods):
+                conditions = mtd.conditions
+                #points = [(c[0]+c[1])/2 for c in conditions]
+                colors = ['Green' if pt else 'Red' for pt in mtd.pts_ok[i]]
+
+                for k, c in enumerate(conditions):
+                    #axis[j+1].hlines(c[2], i, i+1, linestyle='dotted')
+                    #axis[j+1].hlines(c[3], i, i+1, linestyle='dotted')
+                    axis[j+1].fill_between(
+                        [k, k+1],
+                        [c[3], c[3]],
+                        [c[2], c[2]],
+                        color=boxcolor,
+                    )
+
+                xp = np.arange(len(conditions)) + 0.5
+                axis[j+1].scatter(xp, mtd.pts_value[i], color=colors)
+
+                axis[j+1].set_ylim(-0.1, 1.1)
+
+            axis[ncols-1].text(0.5, 0.5,
+                               "\n".join([str(m.identified[i])
+                                          for m in methods]),
+                               fontsize=18,
+                               )
+            no_ticks(axis[ncols-1])
+            no_axis(axis[ncols-1])
+
             figs.append(fig)
         return figs
 
 
 class AverageId(_AtomId):
     def __init__(self, element, specs):
-        self.specs = specs
+        super().__init__(specs)
+
         self.conditions = average_id_config[element]
 
         self._mins = np.array([c[2] for c in self.conditions])
@@ -159,7 +194,8 @@ class AverageId(_AtomId):
 
 class SlopeId(_AtomId):
     def __init__(self, element, specs):
-        self.specs = specs
+        super().__init__(specs)
+
         self.conditions = slope_id_config[element]
 
         self._mins = np.array([c[2] for c in self.conditions])
@@ -167,8 +203,8 @@ class SlopeId(_AtomId):
 
         self.identified = []
         self.lines = []
-        self.fits = []
-        self.fits_ok = []
+        self.pts_value = []
+        self.pts_ok = []
         for s in specs:
             k_V = s.keys.V
             k_I = s.keys.I
@@ -183,8 +219,8 @@ class SlopeId(_AtomId):
             fits_ok = (self._mins <= fits) & (fits <= self._maxs)
 
             self.lines.append(lines)
-            self.fits.append(fits)
-            self.fits_ok.append(fits_ok)
+            self.pts_value.append(fits)
+            self.pts_ok.append(fits_ok)
             self.identified.append(np.all(fits_ok))
         self.identified = np.array(self.identified)
 
@@ -193,6 +229,8 @@ class DoubleId(_AtomId):
         self.specs = specs
         self.average_id = AverageId(element, specs)
         self.slope_id = SlopeId(element, specs)
+
+        self.methods = [self.average_id, self.slope_id]
 
         self.identified = np.array([
             self.average_id.identified,
