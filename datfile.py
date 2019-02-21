b748da4d31c1b862746605b15eb15a6d1b367803
Jeremie <jeremie.gobeil@gmail.com>
2019-02-21 15:23:56 +0100

    Some improvement on BiasSpec

--- datfile.py
+++ datfile.py
@@ -5,15 +5,19 @@ from collections import Counter
 from types import SimpleNamespace
 import io
 import base64
+from collections import ChainMap
 
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 from matplotlib.style import context as mpl_context
+from lazy import lazy
 
 from .helper import get_logger
 from .helper import lazy_property
 from .helper.fileparser import TabHeaderFile
 from .helper.fileparser import Parse
+from .helper.units import unit_factor, unit_names, unit_symbol
 
 from .plotting import get_figsize
 from .plotting import create_figure
@@ -61,51 +65,65 @@ class GenericDatFile(TabHeaderFile):
 class BiasSpec(GenericDatFile):
 
     # columns name to search for in file. Go through a list until a match.
-    channel_names_search_list = {
-        'I': ['Current (A)', ],
-        'V': ['Bias calc (V)', 'Bias (V)', 'Bias w LI (V)', ],
+    search_names = ChainMap({
+        'Current': ['Current (A)', ],
+        'Bias': ['Bias w LI (V)', 'Bias calc (V)', 'Bias (V)', ],
         'LIX': ['Lock-In X (V)',
                 'Lock-in_X (V)',
                 'LI Demod 1 X (A)', ],
         'LIY': ['Lock-In Y (V)',
                 'Lock-in_Y (V)',
                 'LI Demod 1 Y (A)', ],
-    }
+        'LIR': ['Lock-In R (V)',
+                'Lock-in_R (V)',
+                'LI Demod 1 R (A)', ],
+        'Z': ['Z (m)', ],
+        'Field': ['B Field (T)', ],
+    })
 
     # column names for the calculated data
-    calculated_field_names = {
-        'NdIdV': 'NdI/dV (V)',  # numerical dI/dV (from current)
-        'dIdV': 'dI/dV (nA/V)',  # 'normalized' dI/dV
-        'dIdmV': 'dI/dV (nA/mV)',  # 'normalized' dI/dV
-        'dIdV_LI': 'dI/dV (nA/V) (from LI)',
-        'dIdmV_LI': 'dI/dV (nA/mV) (from LI)',
-        'dI_LI_ratio': 'dI_vs_LI_ratio',  # 'ratio between numeric and Lock-In
-        'mV': 'Bias (mV)',  # 'ratio between numeric and Lock-In
-    }
+    field_names = ChainMap({
+        'Current': 'Current [nA]',
+        'Bias': 'Bias [mV]',
+        'Z': 'Z [nm]',
+        'LIX': 'Lock-In X [V]',
+        'LIY': 'Lock-In Y [V]',
+        'LIR': 'Lock-In R [V]',
+        'NdIdV': 'NdI/dV [S]',  # numerical dI/dV (from current)
+        'dIdV': 'dI/dV [μS]',
+        'dIdV_LI': 'dI/dV [μS] (from lock-in)',
+        'dIdV_FIT': 'dI/dV [μS] (from numerical fit)',
+        'Field': 'Field [mT]',
+    })
 
     def __init__(self,
-                 filename,
-                 LI='LIY',
-                 noise_limits=(0.25, 0.75),
-                 LI_sens=50e-3,
-                 LI_amplitude=0.2,
-                 divider=100,
-                 constant_current=False,
+                 filename,  # the field to open
+                 LI='LIY',  # the data channel to use (see BiasSpec.search_names)
+                 default_channel='dIdV',  # default channel to plot
+                 force_numerical_dIdV=False,  # use numerical derivation even if lock in info is availible
+                 noise_limits=(0.25, 0.75),  # limit to consider when fitting the numerical derivation
+                 LI_sensivity=None,  # sensitivity of the lock in
+                 LI_amplitude=None,  # amplitude of the modulation of the lock in
+                 divider=100,  # value of the current divider
+                 constant_current=False,  # if the spectroscopy is at constant current
+                 search_names=None,  # override some search names
+                 field_names=None,  # override some field names
                  ):
         # read file and parse header
         super().__init__(filename)
 
-        # use an attribute so it can be changed on specific file if needed
-        self._cnsl = BiasSpec.channel_names_search_list
-        self._cfn = BiasSpec.calculated_field_names
 
-        # use to cut the current data to limit the noise at high current
-        self.noise_limits = np.array(noise_limits)
-
-        self.LI_sensitivity = LI_sens
+        self.LI = LI
+        self.default_channel = default_channel
+        self.force_numerical_dIdV = force_numerical_dIdV
+        self.noise_limits = np.asarray(noise_limits)
+        self.LI_sens = LI_sensivity
+        self.LI_ampl = LI_amplitude
         self.divider = divider
-        self.LI_amplitude = LI_amplitude
         self.constant_current = constant_current
+        self.search_names = BiasSpec.search_names.new_child(search_names)
+        self.field_names = BiasSpec.field_names.new_child(field_names)
+        self.info = {}
 
         if LI not in ['LIY', 'LIX']:
             self.is_ok = False
@@ -117,32 +135,158 @@ class BiasSpec(GenericDatFile):
             #log.wrn("%s is not as BiasSpec file.", self.filename)
             return
 
-        self.keys = self.infer_keys(LI=LI)
-        log.dbg("Opened: '%s' as BiasSpec (LI: %s; Bias: %s)",
-                self.filename, self.keys.LI, self.keys.V)
+    def set_units(self, name, unit):
+
+        _name = self._find_key_for_name(name)
+
+        fullname = self.field_names[_name]
+        u1 = fullname.find('[')+1
+        u2 = fullname.find(']')
+        oldunit = fullname[u1:u2]
+
+        if len(oldunit) > 1:
+            self.field_names[_name] = "%s%s%s" % (
+                    fullname[:u1], unit, fullname[u1 + 1:])
+        else:
+            self.field_names[_name] = "%s%s%s" % (
+                fullname[:u1], unit, fullname[u1:])
+        try:
+            del self.data
+        except AttributeError:
+            pass
+        try:
+            del self.keys
+        except AttributeError:
+            pass
+
+    def _find_key_for_name(self, name):
+        if name in self.field_names:
+            return name
+        else:
+            for key, fullname in self.field_names.items():
+                if fullname == name:
+                    return key
+        log.err("Field name %s not found.", name)
+        return self.field_names.keys()[0]
+
+    @lazy
+    def rawdata(self):
+        return super().get_data()
+
+    @lazy
+    def data(self):
+        rk = self.rawkeys
+        k = self.keys
+
+        raw = self.rawdata.sort_values(rk.Bias)
+
+        N = raw.shape[0]
+
+        V = raw[rk.Bias]
+        I = raw[rk.Current]
+        LI = raw[rk.LI]
+
+        cal = {}
+
+        dV = (V.max() - V.min()) / (N-1)
+        cal[k.NdIdV] = np.gradient(I, dV, edge_order=2)
+
+        # limits noise issues
+        nl = np.round(self.noise_limits * N).astype(int)
+        ratio_numeric = (cal[k.NdIdV] / LI)[nl[0]:nl[1]].mean()
+
+        if self.LI_sens is not None and self.LI_ampl is not None:
+            ratio_lockin = self.LI_sens / (2*self.LI_ampl)
+        else:
+            ratio_lockin = np.NaN
+
+        cal[k.dIdV_FIT] = LI * ratio_numeric
+        cal[k.dIdV_LI] = LI * ratio_lockin
+
+        if np.isnan(ratio_lockin) or self.force_numerical_dIdV:
+            numerical_dIdV = True
+            cal[k.dIdV] = cal[k.dIdV_FIT]
+        else:
+            numerical_dIdV = False
+            cal[k.dIdV] = cal[k.dIdV_LI]
+
+        rkd = rk.__dict__
+        kd = k.__dict__
+
+        for key, fullname in self.field_names.items():
+            unit = fullname[fullname.find('[')+1:fullname.find(']')]
+            if len(unit) > 1 and unit[0] in unit_factor:
+                factor = unit_factor[unit[0]]
+            else:
+                factor = 1.0
+
+            if kd[key] in cal:
+                cal[kd[key]] = cal[kd[key]] / factor
+            elif key in rkd:
+                cal[kd[key]] = raw[rkd[key]] / factor
+
+        return pd.DataFrame(cal)
+
+    @lazy
+    def keys(self):
+        return SimpleNamespace(**self.field_names)
+
+    @lazy
+    def rawkeys(self):
+        """ Infer the key names from the file header."""
+        k = {}
+        for key, names in self.search_names.items():
+            for name in names:
+                if name in self.channel_names:
+                    k[key] = name
+                    break
+
+        assert 'Current' in k
+        assert 'Bias' in k
+        assert self.LI in k
+
+        k['LI'] = k[self.LI]
+
+        return SimpleNamespace(**k)
+
+    @lazy_property
+    def channel_names(self):
+        """ List of channels names that can be found in file."""
+        header_names = [s.strip() for s in
+                        self.header['Bias Spectroscopy>Channels'].split(';')]
+
+        # 'Bias calc (V)' is in file but not in the header.
+        return ['Bias calc (V)', ] + header_names
 
     def plot(self, title=None, ax=None, save=False, size=None,
-             x=None, y=None, xlabel=None, ylabel=None,
+             x=None, y=None, xlabel=None, ylabel=None, label=None,
              pyplot=True, dpi=100,
              **kwargs,):
         if 'figsize' not in kwargs and ax is None:
             kwargs['figsize'] = get_figsize(size)
 
         if x is None:
-            x = self.keys.mV
+            x = self.keys.Bias
         if y is None:
-            y = self.keys.dIdmV
+            y = self.keys.dIdV
+
+        _x = self.field_names[self._find_key_for_name(x)]
+        _y = self.field_names[self._find_key_for_name(y)]
+
         if xlabel is None:
-            xlabel = 'Bias [mV]'
+            xlabel = _x
         if ylabel is None:
-            ylabel = 'dI/dV [nA/mV]'
+            ylabel = _y
+        if label is None:
+            #label = "%.03d" % self.serie_number
+            label = "%s" % self.fn_noext
 
         if ax is None:
             figure = create_figure(size=size, pyplot=pyplot, dpi=dpi,
                                    shape='golden')
             ax = figure.add_subplot(111)
 
-        ax = self.data.plot(x=x, y=y, ax=ax, **kwargs)
+        ax = self.data.plot(x=_x, y=_y, ax=ax, label=label, **kwargs)
 
         if title is not None:
             ax.set_title(title)
@@ -165,117 +309,6 @@ class BiasSpec(GenericDatFile):
     @property
     def name(self):
         return self.serie_name + "%.3i" % self.serie_number
-
-    def infer_keys(self, LI='LIY'):
-        """ Infer the key names from the file header."""
-        k = {}
-        for key, names in self._cnsl.items():
-            for name in names:
-                if name in self.channel_names:
-                    k[key] = name
-                    break
-        k.update(**self._cfn)
-        if LI in k:
-            k['LI'] = k[LI]
-
-        # Check
-        for field, name in [
-            ('V', 'Bias'),
-            ('I', 'Current'),
-        ]:
-            if field not in k:
-                log.wrn("%s: No '%s' field found.\n"
-                        '\tsearched for :%s\n'
-                        '\tfield in file: %s',
-                        self.filename, name,
-                        self._cnsl[field], self.channel_names)
-        if 'LI' not in k:
-            log.wrn("%s: No 'Lock-In' field.", self.filename)
-            k['LI'] = None
-        return SimpleNamespace(**k)
-
-    @lazy_property
-    def channel_names(self):
-        """ List of channels names that can be found in file."""
-        header_names = [s.strip() for s in
-                        self.header['Bias Spectroscopy>Channels'].split(';')]
-
-        # 'Bias calc (V)' is in file but not in the header.
-        return ['Bias calc (V)', ] + header_names
-
-    def get_data(self):
-        k = self.keys
-
-        df = super().get_data().sort_values(k.V)
-
-        N = len(df[k.V])
-        dV = (df[k.V].max() - df[k.V].min()) / (N-1)
-
-        # limits noise issues
-
-        ml = np.round(self.noise_limits * N).astype(int)
-
-
-        df[k.mV] = df[k.V]*1000
-        df[k.NdIdV] = np.gradient(1e9*df[k.I], dV, edge_order=2)
-        
-        self.ratio_lock_in = self.LI_sensitivity / (2*self.LI_amplitude)
-        self.ratio_numeric = abs((df[k.NdIdV] / df[k.LI])[ml[0]:ml[1]].mean())
-        
-        df[k.dIdV] = df[k.LI] * self.ratio_numeric
-        df[k.dIdmV] = df[k.dIdV] / 1000
-        
-        df[k.dIdV_LI] = df[k.LI] * self.ratio_lock_in 
-        df[k.dIdmV_LI] = df[k.dIdV_LI] / 1000
-        
-        return df
-    
-    @property
-    def ratio_info_str(self):
-        self.data
-        return "%g / %g (num / li) ;  %g / %g (li / est)" % (
-        self.ratio_numeric, self.ratio_lock_in, 
-        self.LI_sensitivity, self.LI_sensitivity_estimation)
-    
-    @property
-    def LI_sensitivity_estimation(self):
-        self.data
-        return self.ratio_numeric*2*self.LI_amplitude 
-
-    def iplot(self):
-        import ipywidgets as w
-        from IPython.display import display
-
-        xselect = w.Select(
-            description='x',
-            options=self.keys.__dict__.values(),
-            value=self.keys.mV,
-        )
-        yselect = w.Select(
-            description='y',
-            options=self.keys.__dict__.values(),
-            value=self.keys.dIdmV,
-        )
-
-        html = w.HTML()
-
-        def change(*args):
-            html.value = "".join([
-            "<img style='display:block;width:600px;'",  # width:100px;height:100px;'
-            "src='data:image/png;base64, ",
-            self.get_base64_plot(
-                x=xselect.value,
-                y=yselect.value,
-            ),
-            "' />",
-            ])
-
-        xselect.observe(change, 'value')
-        yselect.observe(change, 'value')
-
-        change()
-        display(w.VBox([xselect, yselect, html]))
-
 
     def __repr__(self):
         return "%s (%gV .. %gV)" % (
---
+++ helper/units.py
@@ -0,0 +1,33 @@
+unit_factor = {
+    '': 1.0,
+    'm': 1.0e-3,
+    'u': 1.0e-6,
+    'μ': 1.0e-6,
+    'n': 1.0e-9,
+    'p': 1.0e-12,
+    'f': 1.0e-15,
+}
+
+unit_names = {
+    '': "",
+    'm': "milli",
+    'μ': "micro",
+    'n': "nano",
+    'p': "pico",
+    'f': "femto",
+    }
+
+unit_symbol = {
+    '': '',
+    'm': 'm',
+    'milli': 'm',
+    'u': 'μ',
+    'μ': 'μ',
+    'micro': 'μ',
+    'n': 'n',
+    'nano': 'n',
+    'p': 'p',
+    'pico': 'p',
+    'f': 'f',
+    'femto': 'f',
+}
